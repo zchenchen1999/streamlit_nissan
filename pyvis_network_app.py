5722296d607d05b3fbf3078a2955f610b2db05e6
zchenchen1999 <zchenchen1999@gmail.com>
2023-05-17 16:18:56 +0800

    change size

--- pyvis_network_app.py
+++ pyvis_network_app.py
@@ -68,4 +68,4 @@ else:
         HtmlFile = open(f'{path}/pyvis_graph.html', 'r', encoding='utf-8')
 
     # Load HTML file in HTML component for display on Streamlit page
-    components.html(HtmlFile.read(), height=435)
+    components.html(HtmlFile.read(), height=600)
