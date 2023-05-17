c21ff958c9320d0e19496ec3c1114a0859410e4b
zchenchen1999 <zchenchen1999@gmail.com>
2023-05-17 22:24:46 +0800

    change size

---
+++ word_freq_graph.py
@@ -0,0 +1,13 @@
+# 詞頻計算圖
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
+
+# 標題、文字
+st.title("Sentiment Analysis of Tweets about US Airlines")
+st.markdown("This application is a Streamlit dashboard to analyze the sentiment of tweets")
+
+# sidebar 標題、文字
+st.sidebar.title("Sentiment Analysis of Tweets about US Airline")
+st.sidebar.markdown("This application is a Streamlit dashboard to analyze the sentiment of tweets")
