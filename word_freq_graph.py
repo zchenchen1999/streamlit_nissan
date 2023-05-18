f2c291fd2e958b03918a4767279bfc9694a0bff5
zchenchen1999 <zchenchen1999@gmail.com>
2023-05-18 20:42:42 +0800

    store

--- word_freq_graph.py
+++ word_freq_graph.py
@@ -62,7 +62,7 @@ if start_date and end_date and aspect_option:
         # ----------------------------------- 設定每個月出現次數字典 --------------------------------- #
         # 生成日期範圍並轉換為所需的字符串格式
         #
-        date_range = pd.date_range(start="{}-{}".format(start_date.year, start_date.month), end="{}-{}".format(end_date.year, 1 if end_date.month != 12 else end_date.month), freq='M').strftime('%Y-%m').tolist()
+        date_range = pd.date_range(start="{}-{}".format(start_date.year, start_date.month), end="{}-{}".format(end_date.year+1 if end_date.month == 12 else end_date.year , 1 if end_date.month == 12 else end_date.month), freq='M').strftime('%Y-%m').tolist()
         # result_dict = {date: 0 for date in date_range}
         all_aspect_count = pd.DataFrame(date_range, columns=['year_month'])
 
