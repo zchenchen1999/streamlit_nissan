28dc4b23801f297a7aafe8628d8c8e3ef4e45061
zchenchen1999 <zchenchen1999@gmail.com>
2023-06-09 12:20:12 +0800

    update homepage

--- HomePage.py
+++ HomePage.py
@@ -11,14 +11,17 @@ st.set_page_config(page_title="裕日僱主品牌輿情系統", layout="wide")
 # title
 st.title("裕日僱主品牌輿情系統")
 
+# 取資料
 df_list = [[] for i in range(2)]
 # 0:面試趣總體, 1:面試趣汽車公司, 2:比薪水總體, 3:比薪水汽車公司
 df_list[0].append(pd.read_csv('./data/art_count/interview/total.csv').drop(columns='Unnamed: 0'))
 df_list[0].append(pd.read_csv('./data/art_count/interview/each_company.csv').drop(columns='Unnamed: 0'))
 df_list[1].append(pd.read_csv('./data/art_count/salary/total.csv').drop(columns='Unnamed: 0'))
 df_list[1].append(pd.read_csv('./data/art_count/salary/each_company.csv').drop(columns='Unnamed: 0'))
-print(df_list)
+
+# 建立頁籤
 tab_list = st.tabs(["面試趣","比薪水"])
+# 每個頁籤建立內容
 for i in range(2):
     
     tab_list[i].write("總體文章數")
@@ -26,10 +29,6 @@ for i in range(2):
     tab_list[i].write("汽車產業公司解鎖文章數")
     tab_list[i].dataframe(data=df_list[i][1], use_container_width=True, height=600)
     
-    # 產業別
-    # industry_df = 
-    # fig = px.pie(df_list[i][1], values='未解鎖', names='公司', title='未解鎖汽車產業公司佔比')
-    # tab_list[i].plotly_chart(fig, use_container_width=True)
     fig = px.pie(df_list[i][1], values='未解鎖', names='公司', title='未解鎖汽車產業公司佔比')
     tab_list[i].plotly_chart(fig, use_container_width=True)
     fig = px.pie(df_list[i][1], values='已解鎖', names='公司', title='已解鎖汽車產業公司佔比')
